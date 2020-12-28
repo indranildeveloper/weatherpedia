7a1fe52a5fd428ad6678572f1943095d46ffd24d
indranildeveloper <indranilhalder.dev@gmail.com>
2020-12-28 19:50:15 +0530

    Preparing for deployment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const hbs = require("hbs");
 const app = express();
 
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 // Public Static Path
 // console.log(path.join(__dirname, "../public"));
